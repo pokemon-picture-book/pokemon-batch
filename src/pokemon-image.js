5c37da7c2ef8563ad8f5c92f9428dda6d668f040
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-06-03 23:57:06 +0900

    イメージ取得処理を修正

--- src/pokemon-image.js
+++ src/pokemon-image.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 var client = require('cheerio-httpcli');
 
-const pokemonJsonDir = `${process.env.PWD}/../pokemon.json`;
+const POKEMON_JSON_DIR = `${process.env.PWD}/../pokemon.json`;
+const POKEMON_ID = process.argv[2];
 
 const makeDir = (dir) => {
     if (!fs.existsSync(dir)) {
@@ -10,61 +11,34 @@ const makeDir = (dir) => {
     return dir;
 }
 
-/*
-サンプル
-client.download
-    .on('ready', function (stream) {
-        // gif画像以外はいらない
-        if (! /\.gif$/i.test(stream.url.pathname)) {
-            return stream.end();
-        }
-
-        // 各種情報表示
-        console.log(stream.url.href); // => 'http://hogehoge.com/foobar.png'
-        console.log(stream.type);     // => 'image/png'
-        console.log(stream.length);   // => 10240
-
-        // Buffer化してファイルに保存
-        stream.toBuffer(function (err, buffer) {
-            fs.writeFileSync('foobar.png', buffer, 'binary');
-        });
-    })
-    .on('error', function (err) {
-        console.error(err.url + ': ' + err.message);
-    })
-    .on('end', function (err) {
-        console.log('queue is empty');
-    });
-*/
-
 const clientSetup = () => {
     client.download
-        .on('ready', (stream) => {
+        .on('ready', function(stream) {
             const { path } = stream.url;
-            const splitPaths = path.split('/').filter(p => !!p && p !== 'material');
-            const fileName = splitPaths.pop();
+            const pathParts = path.split('/').filter(p => !!p && p !== 'material');
+            const fileName = pathParts.pop();
 
-            console.log(path);
-            console.log('====================');
+            if (([...pathParts].shift() === 'icon' || [...pathParts].shift() === 'overworld') && ! /\.gif$/i.test(fileName)) {
+                return stream.end();
+            }
 
-            // const hrefItems = stream.url.href.split('/');
-            // const filename = hrefItems.pop();
-            // const mainDirName = filename.slice(0, 3);
+            const imageDir = makeDir(`${POKEMON_JSON_DIR}/img`);
+            const pokemonNo = POKEMON_ID.padStart(3, '0');
+            const pokemonImageDir = makeDir(`${imageDir}/${pokemonNo}`);
 
-            // if (filename.split('.').shift().length >= 3 && /^([0-9]*|0)$/.test(mainDirName)) {
-            //     const imgDir = makeDir(`./img`);
-            //     // const imgDir = makeDir(`${pokemonJsonDir}/img`);
-            //     const mainDir = makeDir(`${imgDir}/${mainDirName}`);
-            //     const subDir = makeDir(`${mainDir}/${hrefItems.pop()}`);
+            const savedDir = pathParts.reduce((a, c) => {
+                const subDir = makeDir(`${a}/${c}`);
+                return subDir;
+            }, pokemonImageDir);
 
-            //     stream.pipe(fs.createWriteStream(`${subDir}/${filename}`));
-            //     console.log(stream.url.href + 'をダウンロードしました');
-            // }
+            stream.toBuffer(function (_, buffer) {
+                fs.writeFileSync(`${savedDir}/${fileName}`, buffer, 'binary');
+            });
         })
-        .on('error', (err) => {
+        .on('error', function(err) {
             console.error(err.url + 'をダウンロードできませんでした: ' + err.message);
         })
-        .on('end', () => {
+        .on('end', function() {
             console.log('ダウンロードが完了しました');
         });
 
@@ -72,30 +46,32 @@ const clientSetup = () => {
 }
 
 const imageDownload = () => {
-    [...Array(2)].forEach((_, i) => {
-    // [...Array(721)].forEach((_, i) => {
-        const pokemonNo = String(i + 1).padStart(3, '0');
+    const pokemonNo = POKEMON_ID.padStart(3, '0');
 
-        const { $, error } = client.fetchSync(`http://hikochans.com/pixelart/pokemon/${pokemonNo}`);
-        if (error) {
-            throw error;
-        }
+    const { $, error } = client.fetchSync(`http://hikochans.com/pixelart/pokemon/${pokemonNo}`);
+    if (error) {
+        throw error;
+    }
 
-        $('table.material.material-mini img').download();
-        // $('table.material.material-gen1 img').download();
-        // $('table.material.material-gen2 img').download();
-        // $('table.material.material-gen3 img').download();
-        // $('table.material.material-gen4 img').download();
-        // $('table.material.material-gen5 img').download();
-        // $('table.material.material-gen6 img').download();
-        // $('img.actiongif').download();
-    });
+    $('table.material.material-mini img').download();
+    $('table.material.material-gen1 img').download();
+    $('table.material.material-gen2 img').download();
+    $('table.material.material-gen3 img').download();
+    $('table.material.material-gen4 img').download();
+    $('table.material.material-gen5 img').download();
+    $('table.material.material-gen6 img').download();
+    $('img.actiongif').download();
 }
 
 (async () => {
-    if (!fs.existsSync(pokemonJsonDir)) {
+    if (!fs.existsSync(POKEMON_JSON_DIR)) {
         throw new Error('npm script で実行してください。また pokemon.json プロジェクトをクローンしてください。');
     }
+
+    if (!POKEMON_ID) {
+        throw new Error('コマンドライン引数を指定してください。');
+    }
+
     clientSetup();
     imageDownload();
 })();
